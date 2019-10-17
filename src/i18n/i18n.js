09a58265f20a175c7bbeff803c130f1eb719151c
lurpis <hi@lurpis.com>
2019-10-17 19:39:25 +0800

    Update logo

--- src/i18n/i18n.js
+++ src/i18n/i18n.js
@@ -3,30 +3,29 @@ import { initReactI18next } from "react-i18next";
 
 import resources from "./i18n.json";
 
-let language
+let language = "en"
 if(typeof navigator !== "undefined") {
   language = navigator.language.split(/[-_]/)[0]; // 第一次从浏览器获取用户语言环境
 
   if (navigator.language.split(/[-_]/)[0] !== "zh") {
     language = "en"; // 如果用户不是 zh，那就设置为 en
   }
+}
 
+if(typeof localStorage !== "undefined") {
   if (localStorage.getItem("userLangStorage")) {
     language = localStorage.getItem("userLangStorage"); // 如果用户有设置过，那就从用户设置里调取
   }
-
-  i18n
-  .use(initReactI18next) // passes i18n down to react-i18next
-    .init({
-      resources,
-      lng: language,
-
-      // keySeparator: false, // we do not use keys in form messages.welcome
-
-      interpolation: {
-        escapeValue: false // react already safes from xss
-      }
-    });
 }
+
+i18n
+.use(initReactI18next) // passes i18n down to react-i18next
+  .init({
+    resources,
+    lng: language,
+    interpolation: {
+      escapeValue: false // react already safes from xss
+    }
+  });
 
 export default i18n;
